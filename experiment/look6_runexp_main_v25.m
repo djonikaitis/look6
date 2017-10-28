485217975576e05d825242c9790d6c2db5829dd8
djonikaitis <djonikaitis@yahoo.com>
2017-10-28 14:04:42 -0700

    exp design improvement (#3)
    renamed conditions from numbers to words

--- experiment/look6_runexp_main_v25.m
+++ experiment/look6_runexp_main_v25.m
@@ -4,13 +4,15 @@
 % V2.2 July 15, 2017: Improved compatability with new framework
 % V2.3 August 19, 2017: Finished compatibility with new framework
 % V2.4 August 23, 2017: Added helper functions folder to the path
+% V2.5 October 28, 2017: renamed _settings file into _runexp_settings (to
+% distingish from analysis_settings)
 
 
 close all;
 clear all;
 clc;
 
-fprintf ('\nRun experiment file used is V2.4, update as of August 23, 2017\n\n')
+fprintf ('\nRun experiment file used is V2.5, update as of October 25, 2017\n\n')
  
 global expsetup
 global ni
@@ -97,7 +99,7 @@ elseif expsetup.general.debug==2
     expsetup.general.recordeyes = 0; % 0 - no recording; 1 - yes recording; 2 - ask experimenter
 end
 
-expsetup.general.code_settings = sprintf('%s_settings', expsetup.general.expname); % Path to file containing trial settings
+expsetup.general.code_settings = sprintf('%s_runexp_settings', expsetup.general.expname); % Path to file containing trial settings
 expsetup.general.code_trial = sprintf('%s_trial', expsetup.general.expname); % Path to file containing trial presentation
 
 
